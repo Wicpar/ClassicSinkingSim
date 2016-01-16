5fecf9574ac7a38d9ffec0327c9d2b8baa75626f
Wicpar <frederic.artus@laposte.net>
2016-01-16 17:46:11 +0100

    added copyright notices

--- src/com/wicpar/sinkingsimulatorclassic/Ship.java
+++ src/com/wicpar/sinkingsimulatorclassic/Ship.java
@@ -1,3 +1,38 @@
+/*
+ * Copyright (c) Frederic Artus Nieto 2016.
+ *
+ * Statement of Purpose
+ *
+ * The laws of most jurisdictions throughout the world automatically confer exclusive Copyright and Related Rights (defined below) upon the creator and subsequent owner(s) (each and all, an "owner") of an original work of authorship and/or a database (each, a "Work").
+ *
+ * Certain owners wish to permanently relinquish those rights to a Work for the purpose of contributing to a commons of creative, cultural and scientific works ("Commons") that the public can reliably and without fear of later claims of infringement build upon, modify, incorporate in other works, reuse and redistribute as freely as possible in any form whatsoever and for any purposes, including without limitation commercial purposes. These owners may contribute to the Commons to promote the ideal of a free culture and the further production of creative, cultural and scientific works, or to gain reputation or greater distribution for their Work in part through the use and efforts of others.
+ *
+ * For these and/or other purposes and motivations, and without any expectation of additional consideration or compensation, the person associating CC0 with a Work (the "Affirmer"), to the extent that he or she is an owner of Copyright and Related Rights in the Work, voluntarily elects to apply CC0 to the Work and publicly distribute the Work under its terms, with knowledge of his or her Copyright and Related Rights in the Work and the meaning and intended legal effect of CC0 on those rights.
+ *
+ * 1. Copyright and Related Rights. A Work made available under CC0 may be protected by copyright and related or neighboring rights ("Copyright and Related Rights"). Copyright and Related Rights include, but are not limited to, the following:
+ *
+ *     the right to reproduce, adapt, distribute, perform, display, communicate, and translate a Work;
+ *     moral rights retained by the original author(s) and/or performer(s);
+ *     publicity and privacy rights pertaining to a person's image or likeness depicted in a Work;
+ *     rights protecting against unfair competition in regards to a Work, subject to the limitations in paragraph 4(a), below;
+ *     rights protecting the extraction, dissemination, use and reuse of data in a Work;
+ *     database rights (such as those arising under Directive 96/9/EC of the European Parliament and of the Council of 11 March 1996 on the legal protection of databases, and under any national implementation thereof, including any amended or successor version of such directive); and
+ *     other similar, equivalent or corresponding rights throughout the world based on applicable law or treaty, and any national implementations thereof.
+ *
+ * 2. Waiver. To the greatest extent permitted by, but not in contravention of, applicable law, Affirmer hereby overtly, fully, permanently, irrevocably and unconditionally waives, abandons, and surrenders all of Affirmer's Copyright and Related Rights and associated claims and causes of action, whether now known or unknown (including existing as well as future claims and causes of action), in the Work (i) in all territories worldwide, (ii) for the maximum duration provided by applicable law or treaty (including future time extensions), (iii) in any current or future medium and for any number of copies, and (iv) for any purpose whatsoever, including without limitation commercial, advertising or promotional purposes (the "Waiver"). Affirmer makes the Waiver for the benefit of each member of the public at large and to the detriment of Affirmer's heirs and successors, fully intending that such Waiver shall not be subject to revocation, rescission, cancellation, termination, or any other legal or equitable action to disrupt the quiet enjoyment of the Work by the public as contemplated by Affirmer's express Statement of Purpose.
+ *
+ * 3. Public License Fallback. Should any part of the Waiver for any reason be judged legally invalid or ineffective under applicable law, then the Waiver shall be preserved to the maximum extent permitted taking into account Affirmer's express Statement of Purpose. In addition, to the extent the Waiver is so judged Affirmer hereby grants to each affected person a royalty-free, non transferable, non sublicensable, non exclusive, irrevocable and unconditional license to exercise Affirmer's Copyright and Related Rights in the Work (i) in all territories worldwide, (ii) for the maximum duration provided by applicable law or treaty (including future time extensions), (iii) in any current or future medium and for any number of copies, and (iv) for any purpose whatsoever, including without limitation commercial, advertising or promotional purposes (the "License"). The License shall be deemed effective as of the date CC0 was applied by Affirmer to the Work. Should any part of the License for any reason be judged legally invalid or ineffective under applicable law, such partial invalidity or ineffectiveness shall not invalidate the remainder of the License, and in such case Affirmer hereby affirms that he or she will not (i) exercise any of his or her remaining Copyright and Related Rights in the Work or (ii) assert any associated claims and causes of action with respect to the Work, in either case contrary to Affirmer's express Statement of Purpose.
+ *
+ * 4. Limitations and Disclaimers.
+ *
+ *     No trademark or patent rights held by Affirmer are waived, abandoned, surrendered, licensed or otherwise affected by this document.
+ *     Affirmer offers the Work as-is and makes no representations or warranties of any kind concerning the Work, express, implied, statutory or otherwise, including without limitation warranties of title, merchantability, fitness for a particular purpose, non infringement, or the absence of latent or other defects, accuracy, or the present or absence of errors, whether or not discoverable, all to the greatest extent permissible under applicable law.
+ *     Affirmer disclaims responsibility for clearing rights of other persons that may apply to the Work or any use thereof, including without limitation any person's Copyright and Related Rights in the Work. Further, Affirmer disclaims responsibility for obtaining any necessary consents, permissions or other rights required for any use of the Work.
+ *     Affirmer understands and acknowledges that Creative Commons is not a party to this document and has no duty or obligation with respect to this CC0 or use of the Work.
+ *
+ *
+ */
+
 package com.wicpar.sinkingsimulatorclassic;
 
 import com.wicpar.wicparbase.graphics.Color;
@@ -211,7 +246,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y][x + 1].getMaterial().strength) * strengthmul, 2))
 					springs[0][y][x] = false;
 				else
-					origin.applyForce(force);
+				{
+					if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+							.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y][x + 1].getMass()).add(shipsels[y][x + 1].getPos())) - i)
+							== Math.signum(origin.getPos().distance(shipsels[y][x + 1].getPos()) - i))
+						origin.applyForce(force);
+					else
+						origin.move(origin.getPos().sub(shipsels[y][x + 1].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y][x + 1].getPos()) - i).div(2));
+				}
 			} else if (!origin.getMaterial().isHull)
 				origin.setDamaged(true);
 		}
@@ -246,7 +288,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y + 1][x + 1].getMaterial().strength) * strengthmul, 2))
 					springs[1][y][x] = false;
 				else
-					origin.applyForce(force);
+				{
+					if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+							.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y + 1][x + 1].getMass()).add(shipsels[y + 1][x + 1].getPos())) - i* sqrt2)
+							== Math.signum(origin.getPos().distance(shipsels[y + 1][x + 1].getPos()) - i* sqrt2))
+						origin.applyForce(force);
+					else
+						origin.move(origin.getPos().sub(shipsels[y + 1][x + 1].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y + 1][x + 1].getPos()) - i* sqrt2).div(2));
+				}
 			}
 		}
 		force.set(0, 0, 0);
@@ -279,7 +328,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y + 1][x].getMaterial().strength) * strengthmul, 2))
 					springs[2][y][x] = false;
 				else
-					origin.applyForce(force);
+				{
+					if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+							.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y + 1][x].getMass()).add(shipsels[y + 1][x].getPos())) - i)
+							== Math.signum(origin.getPos().distance(shipsels[y + 1][x].getPos()) - i))
+						origin.applyForce(force);
+					else
+						origin.move(origin.getPos().sub(shipsels[y + 1][x].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y + 1][x].getPos()) - i).div(2));
+				}
 			} else if (!origin.getMaterial().isHull)
 				origin.setDamaged(true);
 		}
@@ -316,7 +372,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 					if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y + 1][x - 1].getMaterial().strength) * strengthmul, 2))
 						springs[3][y][x - 1] = false;
 					else
-						origin.applyForce(force);
+					{
+						if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+								.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y + 1][x - 1].getMass()).add(shipsels[y + 1][x - 1].getPos())) - i* sqrt2)
+								== Math.signum(origin.getPos().distance(shipsels[y + 1][x - 1].getPos()) - i* sqrt2))
+							origin.applyForce(force);
+						else
+							origin.move(origin.getPos().sub(shipsels[y + 1][x - 1].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y + 1][x - 1].getPos()) - i* sqrt2).div(2));
+					}
 				}
 			}
 			force.set(0, 0, 0);
@@ -354,7 +417,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y][x - 1].getMaterial().strength) * strengthmul, 2))
 					springs[0][y][x - 1] = false;
 				else
-					origin.applyForce(force);
+				{
+					if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+							.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y][x - 1].getMass()).add(shipsels[y][x - 1].getPos())) - i)
+							== Math.signum(origin.getPos().distance(shipsels[y][x - 1].getPos()) - i))
+						origin.applyForce(force);
+					else
+						origin.move(origin.getPos().sub(shipsels[y][x - 1].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y][x - 1].getPos()) - i).div(2));
+				}
 			} else if (!origin.getMaterial().isHull)
 				origin.setDamaged(true);
 		}
@@ -389,7 +459,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y - 1][x - 1].getMaterial().strength) * strengthmul, 2))
 					springs[1][y - 1][x - 1] = false;
 				else
-					origin.applyForce(force);
+				{
+					if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+							.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y - 1][x - 1].getMass()).add(shipsels[y - 1][x - 1].getPos())) - i* sqrt2)
+							== Math.signum(origin.getPos().distance(shipsels[y - 1][x - 1].getPos()) - i* sqrt2))
+						origin.applyForce(force);
+					else
+						origin.move(origin.getPos().sub(shipsels[y - 1][x - 1].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y - 1][x - 1].getPos()) - i* sqrt2).div(2));
+				}
 			}
 		}
 		force.set(0, 0, 0);
@@ -422,7 +499,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y - 1][x].getMaterial().strength) * strengthmul, 2))
 					springs[2][y - 1][x] = false;
 				else
-					origin.applyForce(force);
+				{
+					if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+							.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y - 1][x].getMass()).add(shipsels[y][x - 1].getPos())) - i)
+							== Math.signum(origin.getPos().distance(shipsels[y - 1][x].getPos()) - i))
+						origin.applyForce(force);
+					else
+						origin.move(origin.getPos().sub(shipsels[y - 1][x].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y - 1][x].getPos()) - i).div(2));
+				}
 			} else if (!origin.getMaterial().isHull)
 				origin.setDamaged(true);
 		}
@@ -459,7 +543,14 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 					if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y - 1][x + 1].getMaterial().strength) * strengthmul, 2))
 						springs[3][y - 1][x] = false;
 					else
-						origin.applyForce(force);
+					{
+						if (Math.signum(new Vector3d(force).mul(Base.getDelta()).div(origin.getMass()).add(origin.getPos())
+								.distance(new Vector3d(force).negate().mul(Base.getDelta()).div(shipsels[y - 1][x - 1].getMass()).add(shipsels[y - 1][x + 1].getPos())) - i* sqrt2)
+								== Math.signum(origin.getPos().distance(shipsels[y - 1][x + 1].getPos()) - i* sqrt2))
+							origin.applyForce(force);
+						else
+							origin.move(origin.getPos().sub(shipsels[y - 1][x + 1].getPos()).normalize().negate().mul(origin.getPos().distance(shipsels[y - 1][x + 1].getPos()) - i* sqrt2).div(2));
+					}
 				}
 			}
 			force.set(0, 0, 0);
@@ -482,6 +573,24 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 		velA = origin.getVel();
 
 		i = 0;
+		while (++i < w - x && springs[0][y][x + i - 1])
+		{
+			other = shipsels[y][x + i];
+			if (other == null)
+			{
+				springs[0][y][x + i - 1] = false;
+				break;
+			}
+			posB = other.getPos();
+			velB = other.getVel();
+
+			double stretch = posA.distance(posB) - i;
+			double stiff = stiffness;
+			tmp = new Vector3d(posB).sub(posA).normalize().mul(stretch * stiff);
+			force2.set(new Vector3d(velA).sub(velB).mul(dampening));
+			force.add(tmp);
+		}
+		force.add(force2);
 		if (x < w - 1 && y < h)
 		{
 			if (springs[0][y][x])
@@ -515,7 +624,6 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 			force.add(tmp);
 			i++;
 		}
-		force.div(i);
 		force.add(force2);
 		if (x < w - 1 && y < h - 1)
 		{
@@ -594,149 +702,6 @@ public class Ship extends ClassPool implements IDrawable, IDynamical
 					f = force.lengthSquared();
 					if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y + 1][x - 1].getMaterial().strength) * strengthmul, 2))
 						springs[3][y][x - 1] = false;
-					else
-						origin.applyForce(force);
-				}
-			}
-			force.set(0, 0, 0);
-		}
-
-
-		//--------------------------------------------------------------------------------------------------------------
-
-
-		i = 0;
-		while (++i <= x && springs[0][y][x - i])
-		{
-			other = shipsels[y][x - i];
-			if (other == null)
-			{
-				springs[0][y][x - i] = false;
-				break;
-			}
-			posB = other.getPos();
-			velB = other.getVel();
-
-			double stretch = posA.distance(posB) - i;
-			double stiff = stiffness;
-			tmp = new Vector3d(posB).sub(posA).normalize().mul(stretch * stiff);
-			force2.set(new Vector3d(velA).sub(velB).mul(dampening));
-			force.add(tmp);
-		}
-		force.div(i);
-		force.add(force2);
-		if (y < h && x > 0)
-		{
-			if (springs[0][y][x - 1])
-			{
-				f = force.lengthSquared();
-				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y][x - 1].getMaterial().strength) * strengthmul, 2))
-					springs[0][y][x - 1] = false;
-				else
-					origin.applyForce(force);
-			} else if (!origin.getMaterial().isHull)
-				origin.setDamaged(true);
-		}
-		force.set(0, 0, 0);
-
-		i = 1;
-		while (i <= x && i <= y && springs[1][y - i][x - i])
-		{
-			other = shipsels[y - i][x - i];
-			if (other == null)
-			{
-				springs[1][y - i][x - i] = false;
-				break;
-			}
-			posB = other.getPos();
-			velB = other.getVel();
-
-			double stretch = posA.distance(posB) - i * sqrt2;
-			double stiff = stiffness;
-			tmp = new Vector3d(posB).sub(posA).normalize().mul(stretch * stiff);
-			force2.set(new Vector3d(velA).sub(velB).mul(dampening));
-			force.add(tmp);
-			i++;
-		}
-		force.div(i);
-		force.add(force2);
-		if (x > 0 && y > 0)
-		{
-			if (springs[1][y - 1][x - 1])
-			{
-				f = force.lengthSquared();
-				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y - 1][x - 1].getMaterial().strength) * strengthmul, 2))
-					springs[1][y - 1][x - 1] = false;
-				else
-					origin.applyForce(force);
-			}
-		}
-		force.set(0, 0, 0);
-
-		i = 0;
-		while (++i <= y && springs[2][y - i][x])
-		{
-			other = shipsels[y - i][x];
-			if (other == null)
-			{
-				springs[2][y - i][x] = false;
-				break;
-			}
-			posB = other.getPos();
-			velB = other.getVel();
-
-			double stretch = posA.distance(posB) - i;
-			double stiff = stiffness;
-			tmp = new Vector3d(posB).sub(posA).normalize().mul(stretch * stiff);
-			force2.set(new Vector3d(velA).sub(velB).mul(dampening));
-			force.add(tmp);
-		}
-		force.div(i);
-		force.add(force2);
-		if (x < w && y > 0)
-		{
-			if (springs[2][y - 1][x])
-			{
-				f = force.lengthSquared();
-				if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y - 1][x].getMaterial().strength) * strengthmul, 2))
-					springs[2][y - 1][x] = false;
-				else
-					origin.applyForce(force);
-			} else if (!origin.getMaterial().isHull)
-				origin.setDamaged(true);
-		}
-		force.set(0, 0, 0);
-
-		i = 1;
-		if (y > 0)
-		{
-			while (i < w - x && i <= y && springs[3][y - i][x + i - 1])
-			{
-				other = shipsels[y - i][x + i];
-				if (other == null)
-				{
-					springs[3][y - i][x + i - 1] = false;
-					break;
-				}
-				posB = other.getPos();
-				velB = other.getVel();
-
-				double stretch = posA.distance(posB) - i * sqrt2;
-				double stiff = stiffness;
-				tmp = new Vector3d(posB).sub(posA).normalize().mul(stretch * stiff);
-				force2.set(new Vector3d(velA).sub(velB).mul(dampening));
-				force.add(tmp);
-				i++;
-			}
-			force.div(i);
-			force.add(force2);
-			if (x < w - 1 && y > 0)
-			{
-				if (springs[3][y - 1][x])
-				{
-					f = force.lengthSquared();
-					if (f > Math.pow(Math.min(origin.getMaterial().strength, shipsels[y - 1][x + 1].getMaterial().strength) * strengthmul, 2))
-						springs[3][y - 1][x] = false;
 					else
 						origin.applyForce(force);
 				}
